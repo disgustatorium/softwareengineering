2e7ad3aa7d43fffb5871e59a669fdacf66c1590d
lyra-scarlet <118722990+lyra-scarlet@users.noreply.github.com>
2023-05-08 16:49:30 +0100

    Added authentication API

--- api-backend/app.js
+++ api-backend/app.js
@@ -1,38 +1,91 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mysql = require('mysql')
+const crypto = require('crypto');
+const jwt = require("jsonwebtoken");
 
 const app = express()
 const port = 3001;
+const tokenKey = "TotallyLegitKey";
 
 const pool  = mysql.createPool({
     connectionLimit : 10,
     host            : 'localhost',
     user            : 'root',
     database        : 'fitquest'
-})
+});
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+const verifyToken = (req, res, next) => {
+    const token =
+        req.body.token || req.query.token || req.headers["x-access-token"];
 
-app.get('', (req, res) => {
+    if (!token) {
+        return res.status(403).send("A token is required for authentication");
+    }
+    try {
+        const decoded = jwt.verify(token, tokenKey);
+        req.user = decoded.username;
+    } catch (err) {
+        return res.status(401).send("Invalid Token");
+    }
+    return next();
+};
+
+app.post('/register', (req, res) => {
+    let data = req.body;
+    data.password = crypto.createHash('md5').update(data.password).digest('hex');
+    
+    // TODO: Check if registration username is unique
+    // TODO: Check if registration email is unique
+    
     pool.getConnection((err, connection) => {
-        if(err) throw err
-        console.log('connected as id ' + connection.threadId)
-        connection.query('SELECT * from users', (err, rows) => {
-            connection.release()
-
+        if (err) throw err;
+        connection.query("INSERT INTO users SET ?", data, (err, rows) => {
+            connection.release();
+            
             if (!err) {
-                res.send(rows)
+            	let token = jwt.sign({username: data.username}, tokenKey, {expiresIn: "2h"});
+                res.send({"success":true, "token":token});
             } else {
-                console.log(err)
+                console.log(err);
+                res.send({"success":false, "reason":"A database error has occurred."});
             }
-
-            console.log(rows)
         })
     })
-})
+});
+
+app.post('/login', (req, res) => {
+    let data = req.body;
+    data.password = crypto.createHash('md5').update(data.password).digest('hex');
+    
+    pool.getConnection((err, connection) => {
+        if (err) throw err;
+        connection.query("SELECT * FROM users WHERE ?", {"username":data.username}, (err, rows) => {
+            if (!err) {
+            	if (rows.length == 1) {
+            	    if (rows[0].password == data.password) {
+            	        let token = jwt.sign({username: data.username}, tokenKey, {expiresIn: "2h"});
+            	    	res.send({"success":true, "token":token});
+            	    } else { 
+            	    	res.send({"success":false, "reason":"Invalid username and password combination."}) 
+            	    }
+            	} else {
+            	    res.send({"success":false, "reason":"Invalid username and password combination."})
+            	}
+            } else {
+                console.log(err);
+                res.send({"success":false, "reason":"A database error has occurred."});
+            }
+        })
+    })
+});
+
+app.post('/', verifyToken, (req, res) => {
+   res.status(200).send("Welcome "+req.user+"!");
+});
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
