143ebe5cde80d6527933fdb5794fc35512d8d0fc
lyra-scarlet <118722990+lyra-scarlet@users.noreply.github.com>
2023-05-15 15:16:58 +0100

    Clean-up + documentation

--- api-backend/app.js
+++ api-backend/app.js
@@ -19,15 +19,14 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 const verifyToken = (req, res, next) => {
-    const token =
-        req.body.token || req.query.token || req.headers["x-access-token"];
-
+    const token = req.body.token;
     if (!token) {
         return res.status(403).send("A token is required for authentication");
     }
     try {
         const decoded = jwt.verify(token, tokenKey);
-        req.user = decoded.username;
+        req.userID = decoded.userID;
+        req.username = decoded.username;
     } catch (err) {
         return res.status(401).send("Invalid Token");
     }
@@ -36,14 +35,14 @@ const verifyToken = (req, res, next) => {
 
 app.post('/register', (req, res) => {
     let data = req.body;
-    data.password = crypto.createHash('md5').update(data.password).digest('hex');
+    data.data.password = crypto.createHash('md5').update(data.password).digest('hex');
     
     // TODO: Check if registration username is unique
     // TODO: Check if registration email is unique
     
     pool.getConnection((err, connection) => {
         if (err) throw err;
-        connection.query("INSERT INTO users SET ?", data, (err, rows) => {
+        connection.query("INSERT INTO users SET ?", data.data, (err, rows) => {
             connection.release();
             
             if (!err) {
@@ -62,11 +61,11 @@ app.post('/login', (req, res) => {
     
     pool.getConnection((err, connection) => {
         if (err) throw err;
-        connection.query("SELECT * FROM users WHERE ?", {"username":data.username}, (err, rows) => {
+        connection.query("SELECT userID, password FROM users WHERE ?", {"username":data.username}, (err, rows) => {
             if (!err) {
             	if (rows.length == 1) {
             	    if (rows[0].password == data.password) {
-            	        let token = jwt.sign({id: data.userID, username: data.username}, tokenKey, {expiresIn: "2h"});
+            	        let token = jwt.sign({userID: rows[0].userID, username: data.username}, tokenKey, {expiresIn: "2h"});
             	    	res.send({"success":true, "token":token});
             	    } else { 
             	    	res.send({"success":false, "reason":"Invalid username and password combination."}) 
@@ -82,122 +81,13 @@ app.post('/login', (req, res) => {
     })
 });
 
-app.post('/getFoods', verifyToken, (req, res) => {
-    let data = req.body;
-    delete data.token;
-    
-    pool.getConnection((err, connection) => {
-        if (err) throw err;
-        if (data.length == 0) {
-            connection.query("SELECT * FROM foods", (err, rows) => {
-                if (!err) {
-                    res.send({"success":true,"data":rows});
-                } else {
-                    console.log(err);
-                    res.send([{"success":false, "reason":"A database error has occurred."}]);
-                }
-            })
-        } else {
-            connection.query("SELECT * FROM foods WHERE ?", data, (err, rows) => {
-                if (!err) {
-                    res.send({"success":true,"data":rows});
-                } else {
-                    console.log(err);
-                    res.send([{"success":false, "reason":"A database error has occurred."}]);
-                }
-            })
-        }
-    })
-});
-
-app.post('/getFoodRecords', verifyToken, (req, res) => {
-    let data = req.body;
-    
-    pool.getConnection((err, connection) => {
-        if (err) throw err;
-        if (!data.ids) {
-            connection.query("SELECT * FROM foodRecords", (err, rows) => {
-                if (!err) {
-                    res.send({"success":true,"data":rows});
-                } else {
-                    console.log(err);
-                    res.send([{"success":false, "reason":"A database error has occurred."}]);
-                }
-            })
-        } else {
-            connection.query("SELECT * FROM foodRecords WHERE weightRecordId IN (" + connection.escape(data.ids) +  ")", (err, rows) => {
-                if (!err) {
-                    res.send({"success":true,"data":rows});
-                } else {
-                    console.log(err);
-                    res.send({"success":false, "reason":"A database error has occurred."});
-                }
-            })
-        }
-    })
-});
-
-app.post('/getWeightRecords', verifyToken, (req, res) => {
-    let data = req.body;
-    
-    pool.getConnection((err, connection) => {
-        if (err) throw err;
-        if (!data.ids) {
-            connection.query("SELECT * FROM weightRecords", (err, rows) => {
-                if (!err) {
-                    res.send({"success":true,"data":rows});
-                } else {
-                    console.log(err);
-                    res.send([{"success":false, "reason":"A database error has occurred."}]);
-                }
-            })
-        } else {
-            connection.query("SELECT * FROM weightRecords WHERE weightRecordId IN (" + connection.escape(data.ids) +  ")", (err, rows) => {
-                if (!err) {
-                    res.send({"success":true,"data":rows});
-                } else {
-                    console.log(err);
-                    res.send({"success":false, "reason":"A database error has occurred."});
-                }
-            })
-        }
-    })
-});
-
-app.post('/getExerciseRecords', verifyToken, (req, res) => {
-    let data = req.body;
-    
-    pool.getConnection((err, connection) => {
-        if (err) throw err;
-        if (!data.ids) {
-            connection.query("SELECT * FROM exerciseRecords", (err, rows) => {
-                if (!err) {
-                    res.send({"success":true,"data":rows});
-                } else {
-                    console.log(err);
-                    res.send([{"success":false, "reason":"A database error has occurred."}]);
-                }
-            })
-        } else {
-            connection.query("SELECT * FROM exerciseRecords WHERE weightRecordId IN (" + connection.escape(data.ids) +  ")", (err, rows) => {
-                if (!err) {
-                    res.send({"success":true,"data":rows});
-                } else {
-                    console.log(err);
-                    res.send({"success":false, "reason":"A database error has occurred."});
-                }
-            })
-        }
-    })
-});
-
 
 app.post('/recordFood', verifyToken, (req, res) => {
     let data = req.body;
-    
+    data.data.ownerID = req.userID;
     pool.getConnection((err, connection) => {
         if (err) throw err;
-        connection.query("INSERT INTO foodRecords SET ?", data.toSet, (err, rows) => {
+        connection.query("INSERT INTO foodRecords SET ?", data.data, (err, rows) => {
             if (!err) {
                 res.send({"success":true});
             } else {
@@ -210,10 +100,10 @@ app.post('/recordFood', verifyToken, (req, res) => {
 
 app.post('/recordWeight', verifyToken, (req, res) => {
     let data = req.body;
-    
+    data.data.ownerID = req.userID;
     pool.getConnection((err, connection) => {
         if (err) throw err;
-        connection.query("INSERT INTO weightRecords SET ?", data.toSet, (err, rows) => {
+        connection.query("INSERT INTO weightRecords SET ?", data.data, (err, rows) => {
             if (!err) {
                 res.send({"success":true});
             } else {
@@ -226,10 +116,10 @@ app.post('/recordWeight', verifyToken, (req, res) => {
 
 app.post('/recordExercise', verifyToken, (req, res) => {
     let data = req.body;
-    
+    data.data.ownerID = req.userID;
     pool.getConnection((err, connection) => {
         if (err) throw err;
-        connection.query("INSERT INTO exerciseRecords SET ?", data.toSet, (err, rows) => {
+        connection.query("INSERT INTO exerciseRecords SET ?", data.data, (err, rows) => {
             if (!err) {
                 res.send({"success":true});
             } else {
@@ -243,23 +133,90 @@ app.post('/recordExercise', verifyToken, (req, res) => {
 app.post('/getGoals', verifyToken, (req, res) => {
     let data = req.body;
     
-    // TODO
+    pool.getConnection((err, connection) => {
+        if (err) throw err;
+        connection.query("SELECT goals FROM users WHERE ?", {"userID":req.userID}, (err, userRows) => {
+	    connection.query("SELECT * FROM goals WHERE goalID IN (" + userRows[0].goals.slice(1,-1) +  ")", (err, goalRows) => {
+		if (!err) {
+	            res.send({"success":true,"data":goalRows});
+		} else {
+		    console.log(err);
+	            res.send({"success":false, "reason":"A database error has occurred."});
+		}
+	    })
+	})
+    })
 });
 
-app.post('/test', (req, res) => {
+app.post('/getFoods', verifyToken, (req, res) => {
     let data = req.body;
+    delete data.token;
     
     pool.getConnection((err, connection) => {
         if (err) throw err;
-        connection.query("SELECT * FROM weightRecords WHERE weightRecordId IN (" + connection.escape(data.data) +  ")", (err, rows) => {
+        connection.query("SELECT * FROM foods", (err, rows) => {
             if (!err) {
                 res.send({"success":true,"data":rows});
             } else {
                 console.log(err);
-                res.send({"success":false, "reason":"A database error has occurred."});
+                res.send([{"success":false, "reason":"A database error has occurred."}]);
             }
         })
     })
+});
+
+app.post('/getFoodRecords', verifyToken, (req, res) => {
+    let data = req.body;
+    
+    pool.getConnection((err, connection) => {
+        if (err) throw err;
+        connection.query("SELECT * FROM foodRecords WHERE ownerID = "+connection.escape(req.userID), (err, rows) => {
+            if (!err) {
+                res.send({"success":true,"data":rows});
+            } else {
+                console.log(err);
+                res.send([{"success":false, "reason":"A database error has occurred."}]);
+            }
+        })
+    })
+});
+
+app.post('/getWeightRecords', verifyToken, (req, res) => {
+    let data = req.body;
+    
+    pool.getConnection((err, connection) => {
+        if (err) throw err;
+        connection.query("SELECT * FROM weightRecords WHERE ownerID = "+connection.escape(req.userID), (err, rows) => {
+            if (!err) {
+                res.send({"success":true,"data":rows});
+            } else {
+                console.log(err);
+                res.send([{"success":false, "reason":"A database error has occurred."}]);
+            }
+        })
+    })
+});
+
+app.post('/getExerciseRecords', verifyToken, (req, res) => {
+    let data = req.body;
+    
+    pool.getConnection((err, connection) => {
+        if (err) throw err;
+        connection.query("SELECT * FROM exerciseRecords WHERE ownerID = "+connection.escape(req.userID), (err, rows) => {
+            if (!err) {
+                res.send({"success":true,"data":rows});
+            } else {
+                console.log(err);
+                res.send([{"success":false, "reason":"A database error has occurred."}]);
+            }
+        })
+    })
+});
+
+app.post('/test', verifyToken, (req, res) => {
+    let data = req.body;
+    
+    //
 });
 
 
---
+++ api-backend/documentation.md
@@ -0,0 +1,33 @@
+/register {data:{username, password, email, firstName, lastName, gender, units, height, dob}}
+Registers a user.
+Returns {success:true} if successful, and {success:false, reason} if not.
+
+/login {username,password}
+Attempts to a log in a user.
+
+/recordFood {token,data:{dateRecorded, timeRecorded, foodType, quantity}}
+Records a food.
+Returns {success:true} if successful, and {success:false, reason} if not.
+
+/recordWeight {token,data:{dateRecorded, timeRecorded, weight}}
+Records a weight.
+Returns {success:true} if successful, and {success:false, reason} if not.
+
+/recordExercise {token,data:{dateRecorded, timeRecorded, hours}}
+Records an exercise.
+Returns {success:true} if successful, and {success:false, reason} if not.
+
+/getGoals {token}
+Returns an array of the user's goals as JSON: {success, data}
+
+/getFoods {token}
+Returns an array of all food types in the DB as JSON: {success, data}
+
+/getFoodRecords {token}
+Returns an array of all the user's weight records in the DB as JSON: {success, data}
+
+/getWeightRecords {token}
+Returns an array of all the user's weight records in the DB as JSON: {success, data}
+
+/getExerciseRecords {token}
+Returns an array of all the user's weight records in the DB as JSON: {success, data}
