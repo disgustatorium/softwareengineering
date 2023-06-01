713ce3acb8a50a81332ff9cbb6084b9a594e0369
charkharris <charris.pers@gmail.com>
2023-06-01 23:29:54 +0100

    settings start frontend

--- react-app/src/routes/user/appSettings.jsx
+++ react-app/src/routes/user/appSettings.jsx
@@ -2,16 +2,10 @@ import { useState } from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
 import Grid from '@mui/material/Unstable_Grid2';
 import { Button } from '@mui/material';
-import { Link } from 'react-router-dom';
-import dayjs from 'dayjs';
-import { DatePicker } from '@mui/x-date-pickers';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
-import RestaurantIcon from '@mui/icons-material/Restaurant';
-import RamenDiningIcon from '@mui/icons-material/RamenDining';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -28,7 +22,7 @@ export default function AppSettings() {
     var formData = { "newEmail":newEmail.target.textContent};
 
     let requestJson = {"token":window.localStorage.getItem("token"),"data":formData};
-    fetch('http://localhost:3001/recordFood',{method:'POST',body:JSON.stringify(requestJson),headers:{'Content-type':'application/json; charset=UTF-8'},}).then((response) => response.json()).then((data) => {
+    fetch('http://localhost:3001/appSettings',{method:'POST',body:JSON.stringify(requestJson),headers:{'Content-type':'application/json; charset=UTF-8'},}).then((response) => response.json()).then((data) => {
         if (data.success) window.location.href = "../addSuccess";
         else console.log(data);
     }).catch((err) => {console.log(err.message);});
@@ -37,10 +31,10 @@ export default function AppSettings() {
     return (
       <Container sx={
         { padding: "30px" }
-      } maxWidth="sm">
+       } maxWidth="sm">
         
         <Typography variant="h2" component="h1" gutterBottom>Settings</Typography>
-            
+            <Item>
             <Grid container direction="column" rowGap={2} maxWidth="sm">
                   
             <TextField 
@@ -51,7 +45,7 @@ export default function AppSettings() {
         />
         <Button variant="contained" onClick={submitChangeEmail}> Change Email</Button>
         </Grid>
-      
+       </Item>
     </Container>
     );
   }
