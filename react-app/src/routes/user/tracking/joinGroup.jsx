1d97658a4887f7f1c6cb5e8d8206ad94ae106008
MillieAbbs <78388108+MillieAbbs@users.noreply.github.com>
2023-06-02 10:43:08 +0100

    Updated styling on join groups page

--- react-app/src/routes/user/tracking/joinGroup.jsx
+++ react-app/src/routes/user/tracking/joinGroup.jsx
@@ -14,42 +14,41 @@ const Item = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
     padding: theme.spacing(2),
     color: theme.palette.text.secondary,
-  }));
+}));
 
 export default function JoinGroup() {
     const [name, setName] = useState();
     const [text, setText] = useState();
-    
+
     function newGroup() {
-	if (!name.target.value) {
-	  alert("Please ensure all fields are filled.");
-	  return;
-	}        
-        let requestJson = {"token":userToken, "groupID":name.target.value};
-        fetch('http://localhost:3001/joinGroup',{method:'POST',body:JSON.stringify(requestJson),headers:{'Content-type':'application/json; charset=UTF-8'},}).then((response) => response.json()).then((data) => {
+        if (!name.target.value) {
+            alert("Please ensure all fields are filled.");
+            return;
+        }
+        let requestJson = { "token": userToken, "groupID": name.target.value };
+        fetch('http://localhost:3001/joinGroup', { method: 'POST', body: JSON.stringify(requestJson), headers: { 'Content-type': 'application/json; charset=UTF-8' }, }).then((response) => response.json()).then((data) => {
             if (data.success) window.location.href = "../groups";
             else setText(data.reason);
-        }).catch((err) => {console.log(err.message);});
+        }).catch((err) => { console.log(err.message); });
     }
 
     return (
-	<Container sx={
-	    { padding: "30px", marginBottom: "76px" }
-	} maxWidth="sm">
-	    
-	    <Typography variant="h2" component="h1" gutterBottom> Join Group </Typography>
-	        <Item>
-	        <Grid container direction="column" rowGap={2} maxWidth="sm">
-            <TextField 
-                label="Group Join ID"
-                variant="outlined"
-                onChange={(newValue) => setName(newValue)}
-            />
-              {text}
-	      <Button variant="contained" onClick={newGroup} endIcon={<AddCircleIcon />}> Join </Button>
-	    </Grid>
-	    </Item>
-	  
-	</Container>
+        <Container sx={
+            { padding: "30px", marginBottom: "76px" }
+        } maxWidth="sm">
+            <Item>
+                <Typography variant="h2" component="h1" gutterBottom> Join Group </Typography>
+                <Grid container direction="column" rowGap={2} maxWidth="sm">
+                    <TextField
+                        label="Group Join ID"
+                        variant="outlined"
+                        onChange={(newValue) => setName(newValue)}
+                    />
+                    {text}
+                    <Button variant="contained" onClick={newGroup} endIcon={<AddCircleIcon />}> Join </Button>
+                </Grid>
+            </Item>
+
+        </Container>
     );
 }
